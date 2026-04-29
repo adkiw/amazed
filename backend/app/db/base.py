4cb28a471bcb3c48b5846df44b24f4a57918adb4
adkiw <DARIUS.AUKSELIS@GMAIL.COM>
2026-04-29 14:22:26 +0300

    Revert "Add Proof-of-First-Capture MVP: backend, frontend, extension, Docker and migrations"

--- backend/app/db/base.py
+++
@@ -1,5 +0,0 @@
-from app.models.media_item import MediaItem
-from app.models.similarity_match import SimilarityMatch
-from app.models.user import User
-
-__all__ = ["User", "MediaItem", "SimilarityMatch"]
